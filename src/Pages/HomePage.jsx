b8b968c87aabfcc90228c01c1addea7a3f9267a1
Vaughn Carroll <Vorno2000@gmail.com>
2024-04-18 17:39:47 +1000

    Image path fixes

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -12,7 +12,7 @@ function HomePage() {
           <div className={styles["image-background"]}>
             <img
               className={styles["profile-image"]}
-              src="./src/Assets/selfpicture.JPG"
+              src="/src/Assets/selfpicture.JPG"
               alt="logo"
             />
           </div>
